38a7d64f302671b9317e056648fc546d8d3c4d5a
Archer Huang <mmosconii@gmail.com>
2021-10-01 13:09:59 +0800

    Update

---
+++ precache-manifest.6121153e67f168b0ad7195478f90b78e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b7abc1d66ea0adc9c2f9",
+    "url": "js/chunk-vendors.66cf36d1.js"
+  },
+  {
+    "revision": "a4fcb8bd2d70f8a48865",
+    "url": "js/app.cb6c4109.js"
+  },
+  {
+    "revision": "f57e947562f7c32db3ed7b37f392a3f5",
+    "url": "index.html"
+  },
+  {
+    "revision": "0a267abaf4c9d30b8fd7c77cc19625f3",
+    "url": "img/zwicon.0a267aba.svg"
+  },
+  {
+    "revision": "d7e439cec483969db045235b2bd3a748",
+    "url": "fonts/zwicon.d7e439ce.ttf"
+  },
+  {
+    "revision": "cbeae0335fc8290a7611dd00a840391f",
+    "url": "fonts/zwicon.cbeae033.woff"
+  },
+  {
+    "revision": "64bead59d217d53c5331dcb3d7687814",
+    "url": "fonts/zwicon.64bead59.eot"
+  },
+  {
+    "revision": "b7abc1d66ea0adc9c2f9",
+    "url": "css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "a4fcb8bd2d70f8a48865",
+    "url": "css/app.9487bd4f.css"
+  }
+];
